2d6019b08683174c5566ccbaef6c5441c8578600
LucaFetz <luca.fetz@epfl.ch>
2019-02-27 15:18:00 +0100

    Robotics practicals seance 1

---
+++ build/ros_basics_2019/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/luca/ros_practicals_ws/src/ros_basics_2019
+# Build directory: /home/luca/ros_practicals_ws/build/ros_basics_2019
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
